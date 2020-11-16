5965da70cf41e60dc00586be513d70b074a29f4a
Krzysztof Siwak <krzysztof.siwak1@gmail.com>
2020-11-17 00:53:02 +0100

    add to cart logic

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,14 +7,16 @@ import {
 	Button,
 	StyleSheet
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductDetailScreen = props => {
 	const productId = props.navigation.getParam('productId');
 	const selectedProduct = useSelector(state =>
 		state.products.availableProducts.find(prod => prod.id === productId)
 	);
+	const dispatch = useDispatch();
 	return (
 		<ScrollView>
 			<Image
@@ -22,7 +24,13 @@ const ProductDetailScreen = props => {
 				style={styles.productImage}
 			/>
 			<View style={styles.actions}>
-				<Button color={Colors.primary} title="To Cart" onPress={() => {}} />
+				<Button
+					color={Colors.primary}
+					title="To Cart"
+					onPress={() => {
+						dispatch(cartActions.addToCart(selectedProduct));
+					}}
+				/>
 			</View>
 			<Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
 			<Text style={styles.description}>{selectedProduct.description}</Text>
